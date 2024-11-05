9287c6a45db65693b14a41332c3fe8b0ee4c4bda
Ryan Gwalchmei <60650064+ryangalvaogp@users.noreply.github.com>
2024-11-05 14:23:22 +0000

    add pages

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+export default function Home() {
+    return (
+        <>
+            <h1>Uma mensagem para meu chuchu</h1>
+            <p>Você é tudo que eu poderia sonhar,<br/>
+                uma namorada linda e perfeita para amar.<br/>
+                Você é tudo que eu sempre quis,<br/>
+                alguém querida e sincera e que me faz feliz.</p>
+
+            <p>Por isso, sempre agradeço por ter você
+                ao meu lado<br/>
+                e por poder ser seu namorado.<br/>
+                Nunca esqueça que você é tudo
+                que importa para mim!</p>
+            <br/>
+            <h2>Te lovo mais que tudo, meu amor! ❤️ </h2>
+            <br/>
+           <strong>De:</strong><i> Ryan</i><br/>
+           <strong>Para:</strong><i> Letícia</i>
+           version 1
+        </>
+    )
+
+}

85885fac48c3d9cc76a70cc434df7c55f2be8231
Ryan Gwalchmei <ryan.gwalchmei@outlook.com.br>
2024-12-17 16:26:48 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,11 @@
-function status(req, res) {
+import database from '../../../../infra/database.js';
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1+1 as sum;");
+
+  console.log(result.rows);
+
+
   res.status(200).json({ message: "Alunos do curso.dev são pessoas acima da média" });
 }
 

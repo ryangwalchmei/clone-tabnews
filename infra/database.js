4e8eaf710e7c74324add46bfb2807832c34135de
Ryan Gwalchmei <ryan.gwalchmei@outlook.com.br>
2025-01-04 21:04:10 -0300

    Add a more robust SSL configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,17 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: true
-  });
+    ssl: getSSLValues(),
 
-  console.log({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
   });
-
 
   try {
     await client.connect();
@@ -34,3 +26,14 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}

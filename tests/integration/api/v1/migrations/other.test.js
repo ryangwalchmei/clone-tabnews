ac727517ef4cabbccdae067e3eec6c70388cfdfe
Ryan Gwalchmei <ryan.gwalchmei@outlook.com.br>
2025-03-05 17:43:55 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/other.test.js
+++ tests/integration/api/v1/migrations/other.test.js
@@ -6,37 +6,43 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("DELETE to /api/v1/migrations should return 405", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "DELETE",
-  });
-  expect(response.status).toBe(405);
-});
+describe("API /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    const url = "http://localhost:3000/api/v1/migrations";
 
-test("PUT to /api/v1/migrations should return 405", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "PUT",
-  });
-  expect(response.status).toBe(405);
-});
+    describe("Use DELETE method", () => {
+      test("should return 405", async () => {
+        const response = await fetch(url, { method: "DELETE" });
+        expect(response.status).toBe(405);
+      });
+    });
 
-test("HEAD to /api/v1/migrations should return 405", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "HEAD",
-  });
-  expect(response.status).toBe(405);
-});
+    describe("Use PUT method", () => {
+      test("should return 405", async () => {
+        const response = await fetch(url, { method: "PUT" });
+        expect(response.status).toBe(405);
+      });
+    });
 
-test("OPTIONS to /api/v1/migrations should return 405", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "OPTIONS",
-  });
-  expect(response.status).toBe(405);
-});
+    describe("Use HEAD method", () => {
+      test("should return 405", async () => {
+        const response = await fetch(url, { method: "HEAD" });
+        expect(response.status).toBe(405);
+      });
+    });
 
-test("PATCH to /api/v1/migrations should return 405", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "PATCH",
+    describe("Use OPTIONS method", () => {
+      test("should return 405", async () => {
+        const response = await fetch(url, { method: "OPTIONS" });
+        expect(response.status).toBe(405);
+      });
+    });
+
+    describe("Use PATCH method", () => {
+      test("should return 405", async () => {
+        const response = await fetch(url, { method: "PATCH" });
+        expect(response.status).toBe(405);
+      });
+    });
   });
-  expect(response.status).toBe(405);
 });

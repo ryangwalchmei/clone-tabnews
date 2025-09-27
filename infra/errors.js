1281353e34a33d8b604a08cc271428c44b0559a8
Ryan Gwalchmei <ryan.gwalchmei@outlook.com.br>
2025-09-27 20:49:47 -0300

    feat: create and use `NotFoundError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -1,5 +1,5 @@
 export class InternalServerError extends Error {
-  constructor({ cause, statusCode }) {
+  constructor ({ cause, statusCode }) {
     super("Um erro interno não esperado aconteceu.", {
       cause,
     });
@@ -19,7 +19,7 @@ export class InternalServerError extends Error {
 }
 
 export class ServiceError extends Error {
-  constructor({ cause, message }) {
+  constructor ({ cause, message }) {
     super(message || "Serviço indisponível no momento.", {
       cause,
     });
@@ -39,7 +39,7 @@ export class ServiceError extends Error {
 }
 
 export class ValidationError extends Error {
-  constructor({ cause, message, action }) {
+  constructor ({ cause, message, action }) {
     super(message || "Um erro de validação ocorreu.", {
       cause,
     });
@@ -58,8 +58,29 @@ export class ValidationError extends Error {
   }
 }
 
+export class NotFoundError extends Error {
+  constructor ({ cause, message, action }) {
+    super(message || "Não foi possível encontrar este recurso no sistema.", {
+      cause,
+    });
+    this.name = "NotFoundError";
+    this.action =
+      action || "Verifique se os parâmetros enviados na consulta estão certos.";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
 export class MethodNotAllowedError extends Error {
-  constructor() {
+  constructor () {
     super("Método não permitido para este endpoint.");
     this.name = "MethodNotAllowedError";
     this.action =

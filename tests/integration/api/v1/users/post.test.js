6705d4c641b7b13be2c70b9af1fb4d6b8ef68bf5
Ryan Gwalchmei <ryan.gwalchmei@outlook.com.br>
2025-10-08 16:50:54 -0300

    feat: add default feature `read:activation_token` when creating `user`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -33,7 +33,7 @@ describe("POST /api/v1/users", () => {
         username: "ryangwalchmei",
         email: "contato@gwalchmei.com.br",
         password: responseBody.password,
-        features: [],
+        features: ["read:activation_token"],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
